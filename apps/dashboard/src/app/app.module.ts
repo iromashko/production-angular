5b90df57da20521a93f4a09b4f6849169cbb55e0
iromashko <iromashko@me.com>
2021-03-10 13:45:38 +0300

    part 2

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,14 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
+import { CoreDataModule } from '@production-angular/core-data';
+import { CoreStateModule } from '@production-angular/core-state';
+import { MaterialModule } from '@production-angular/material';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing.module';
+import { WidgetsDetailsComponent } from './widgets/widgets-details/widgets-details.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+import { WidgetsListComponent } from './widgets/widgets/widgets-list/widgets-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    WidgetsComponent,
+    WidgetsDetailsComponent,
+    WidgetsListComponent,
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    CoreDataModule,
+    CoreStateModule,
+    HttpClientModule,
+    MaterialModule,
+    RoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
